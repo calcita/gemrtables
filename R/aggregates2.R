7966d9feeae8961fd508dc63a79d4b5eaf1afe26
Alasdair McWilliam <crumb.duck@yahoo.co.uk>
2018-08-14 21:28:01 +0200

    resolve duplicates and missing variables.

--- R/aggregates2.R
+++ R/aggregates2.R
@@ -1,9 +1,20 @@
+agg_preprocess <- function(df) {
+  df %>%
+    mutate(value = case_when(
+       ind == "Comp.02" | ind == "Free.02"  ~ as.numeric(value >= 1),
+       ind == "Comp.2t3" ~ as.numeric(value >= 9),
+       ind == "Free.2t3" ~ as.numeric(value >= 12),
+       ind ==  "stu.bully" ~ as.numeric(value == 3),
+       stringr::str_detect(ind, "esd") ~ as.numeric(value >= 2)))
+}
+
 compute_aggregate <- function(df, region, entity) {
 
   region <- as.name(region)
   entity <- as.character(entity)
 
   computed <- df %>%
+    agg_preprocess() %>%
     dplyr::group_by(!!region) %>%
     dplyr::mutate(count1 = n_distinct(iso2c)) %>%
     dplyr::ungroup() %>%
@@ -12,40 +23,32 @@ compute_aggregate <- function(df, region, entity) {
                      median = median(value, na.rm = TRUE),
                      w_mean = sum(value*wt_value, na.rm = TRUE)/sum(wt_value, na.rm = TRUE),
                      pc_true = (sum(value)/dplyr::n())*100,
-                     pc_1 = (sum(ifelse(value >= 1, 1, 0))/dplyr::n())*100,
-                     pc_2 = (sum(ifelse(value >= 2, 1, 0))/dplyr::n())*100,
-                     pc_9 = (sum(ifelse(value >= 9, 1, 0))/dplyr::n())*100,
-                     pc_12 = (sum(ifelse(value >= 12, 1, 0))/dplyr::n())*100,
                      count2 = dplyr::n()) %>%
     dplyr::mutate(pc_comp = 100 * round(count2/count1, digits = 2),
                   entity = entity) %>%
-    tidyr::gather(key = "aggregation", value = "value", -ind, -!!region, -pc_comp, -pc_comp_cut, -as.name(entity)) %>%
+    tidyr::gather(key = "aggregation", value = "value", -ind, -!!region, -pc_comp, -pc_comp_cut, -entity) %>%
     dplyr::select(annex_name = !!region, ind, aggregation, value, pc_comp, pc_comp_cut, entity)
-
 }
-
 
 aggregates <- function(df, region) {
 
-  region <- as.name(region)
+  region <- as.character(region)
 
- if(region == "SDG.region") {
-   subregion == as.name("SDG.subregion")
- }else if(region == "UIS.region") {
-   subregion == as.name("UIS.subregion")
- }else if(region == "GEMR.region") {
-   subregion == as.name("GEMR.subregion")
- }
-
+ #if(region == "SDG.region") {
+ #  subregion == as.name("SDG.subregion")
+ #}else if(region == "UIS.region") {
+  # subregion == as.name("UIS.subregion")
+# }else if(region == "GEMR.region") {
+ #  subregion == as.name("GEMR.subregion")
+# }
 
   world <- compute_aggregate(df, region = "World", entity = "world")
-  regional <- compute_aggregate(df, region = region)
-  subregional <- compute_aggregate(df, region = subregion)
-  income <- compute_aggregate(df, region = "income_group")
+  regional <- compute_aggregate(df, region = region, entity = "region")
+  #subregional <- compute_aggregate(df, region = subregion, entity = "subregion")
+  income <- compute_aggregate(df, region = "income_group", entity = "income_group")
 
-  aggregates <- dplyr::bind_rows(world, regional, subregional, income) %>%
-    dplyr::semi_join(indicators, by = c("ind", "aggregation")) %>%
+  aggregates <- dplyr::bind_rows(world, regional, income) %>%
+    dplyr::semi_join(pkg.env$indicators, by = c("ind", "aggregation")) %>%
     dplyr::mutate(year = ref_year) %>%
-    dplyr::filter(pc_comp >= pc_comp_cut)
-
+    mutate(value = ifelse(pc_comp >= pc_comp_cut, value, NA))
 }
