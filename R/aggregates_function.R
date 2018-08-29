e94e4fb36b497fefbdb9244002fe31b9420864af
Bilal Barakat <bifouba@Bilals-MacBook-Pro.local>
2018-08-29 21:13:15 +0200

    fixed missing quotation marks

--- R/aggregates_function.R
+++ R/aggregates_function.R
@@ -50,8 +50,8 @@ aggregates <- function(df) {
     dplyr::filter(!(aggregation != 'w_mean' & pc_comp < pc_comp_cut) &
                   !(aggregation == 'w_mean' & wt_share < pc_comp_cut)) %>%
     dplyr::mutate(val_status = case_when(
-      aggregation == median & pc_comp < 2/3 ~ 'E',
-      aggregation == wt_mean & wt_share < 2/3 ~ 'E',
+      aggregation == 'median' & pc_comp < 2/3 ~ 'E',
+      aggregation == 'wt_mean' & wt_share < 2/3 ~ 'E',
       TRUE = "A"
     ))
     dplyr::filter(!is.na(annex_name) | annex_name != "")
